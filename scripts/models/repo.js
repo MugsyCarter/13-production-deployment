1bbfa324676911755af0e57b2b0e9e55d14e2520
Mugsy Carter <mugsycarter@gmail.com>
2016-10-05 13:34:44 -0700

    We talked the TODOs in server.js and repo.js but we don't yet have an operational blog.

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,13 +4,21 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    // TODO: refactor this request into an $.ajax call
-    $.get('/github/users/codefellows-portland-301d6/repos' +
-          '?per_page=10&sort=updated')
-          .done(function(data) {
-            reposObj.allRepos = data;
-          }).done(callback);
-  };
+    // DONE: TODO: refactor this request into an $.ajax call
+
+    $.ajax({
+      url: '/github/users/codefellows-portland-301d6/repos' +
+          '?per_page=10&sort=updated',
+          type: 'GET',
+          sucess: callback()
+        });
+
+  //   $.get('/github/users/codefellows-portland-301d6/repos' +
+  //         '?per_page=10&sort=updated')
+  //         .done(function(data) {
+  //           reposObj.allRepos = data;
+  //         }).done(callback);
+  // };
 
   reposObj.withTheAttribute = function(attr) {
     return reposObj.allRepos.filter(function(aRepo) {
