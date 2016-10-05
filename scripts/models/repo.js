ce10e181389b38d528e86cc6a456f89b4bf6cc68
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-10-05 14:22:20 -0700

    Set up app on heroku and set up GITHUB_TOKEN as env

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -9,16 +9,10 @@
     $.ajax({
       url: '/github/users/codefellows-portland-301d6/repos' +
           '?per_page=10&sort=updated',
-          type: 'GET',
-          sucess: callback()
-        });
-
-  //   $.get('/github/users/codefellows-portland-301d6/repos' +
-  //         '?per_page=10&sort=updated')
-  //         .done(function(data) {
-  //           reposObj.allRepos = data;
-  //         }).done(callback);
-  // };
+      type: 'GET',
+      sucess: callback()
+    });
+  };
 
   reposObj.withTheAttribute = function(attr) {
     return reposObj.allRepos.filter(function(aRepo) {
